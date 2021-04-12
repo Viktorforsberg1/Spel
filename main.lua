740f5d7e1d49937767a799dd687d43fbc76b487b
Viktorforsberg1 <65647345+Viktorforsberg1@users.noreply.github.com>
2021-04-12 23:56:55 +0200

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,20 +1,33 @@
+love.window.setTitle("Hejsan")
+
 function love.load()
+
+    totalwidth = 800
+    totalheigh = 600
+
     player = {}
     player.x = 0
     player.y = 0
-    player.Width = 64
-    player.Height = 64
+    player.w = 64
+    player.h = 64
+    player.points = 10
 
     enemy = {}
-    enemy.x = 251
-    enemy.y = 250
+    enemy.x = math.random(0, 800)
+    enemy.y = math.random(0, 600)
     enemy.w = 50
     enemy.h = 50
 
-image = love.graphics.newImage("player.png")
+    time = 0
+
 end
 
 function love.update(dt)
+
+    if player.points > 0 then
+        time = time + dt
+    end
+
     if love.keyboard.isDown("right") then
         player.x = player.x + 2
     elseif love.keyboard.isDown("left") then
@@ -25,13 +38,46 @@ function love.update(dt)
         player.y = player.y  + 2
     end
 
+    if collision(player.x, player.y, player.w, player.h, enemy.x, enemy.y, enemy.w, enemy.h) then
+        player.points = player.points - 1
+        enemy.x = math.random(0, 800)
+        enemy.y = math.random(0,600)
+
+    end
+
+
+    if enemy.x > 750 then
+        enemy.x = enemy.x - 1
+    end
+
+    if enemy.y > 550 then
+        enemy.y = enemy.y - 1
+    end
+
 end
 
 function love.draw()
-    
-    love.graphics.rectangle("fill", 345, 256, 34, 63)
     love.graphics.rectangle("fill", enemy.x, enemy.y, enemy.w, enemy.h)
-    love.graphics.draw(image, player.x, player.y)
-    
-    --love.graphics.print("gameover", 250, 250, 0)
+    love.graphics.rectangle("fill", player.x, player.y, player.w, player.h)
+    love.graphics.print(player.points .. " Kvar", 20, 20)
+    love.graphics.print(math.floor(time), 100, 20)
+
+
+    if win (player.points) then
+        love.graphics.print("You won!", 390, 280)
+        love.graphics.print(time, 390, 300)
+    end
+
 end
+
+function collision(ax, ay, aw, ah, bx, by, bw, bh)
+    return ax < (bx + bw) and
+    bx < (ax + aw) and
+    ay < (by + bh) and
+    by < (ay + ah)
+    end
+
+function win(a)
+    return a < 1
+end
+
